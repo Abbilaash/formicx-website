ad84e062f076dd873df4ba3cd8ebe0decef1362d
Abbilaash <abbilaashat@gmail.com>
2026-09-15 00:17:10 +0530

    formicx-os.iso update

--- config/site.ts
+++ config/site.ts
@@ -1,5 +1,5 @@
 export const SITE_CONFIG = {
-  osDownloadUrl: 'https://drive.google.com/uc?export=download&id=YOUR_DRIVE_ID', // Replace with your Google Drive / GitHub Release link
+  osDownloadUrl: 'https://drive.google.com/file/d/1cImLzWIANFkRYlOYHdrcfOExbkW4A0qH/view?usp=sharing', // Replace with your Google Drive / GitHub Release link
   githubUrl: 'https://github.com/Abbilaash/Formicx',
   osVersion: 'v0.1.0-alpha',
 }
